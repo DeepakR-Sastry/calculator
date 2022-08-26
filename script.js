e151e9934469f8745e2a0a95f918ffd9e3ed3c4d
DeepakR-Sastry <mamillap@ualberta.ca>
2022-08-26 10:30:33 -0600

    added most functionality

--- script.js
+++ script.js
@@ -14,34 +14,8 @@ function divide(a,b){
     return a/b;
 }
 
-function askNumInput(){
-    while(true){
-        let x = prompt("Enter number:");
-        x = parseInt(x);
-        let isNotNum = isNaN(x);
-        if (isNotNum){
-            alert("Not a number! Enter a number");
-        }
-        else{
-            return x;
-        }
-
-    }
-}
-
-function askOpInput(){
-    let operations = ["+", "-", "*", "/"];
-    while(true){
-        op = prompt("Enter operator:");
-        if(operations.includes(op)){
-            return op;
-        }
-        else{
-            alert("Not a valid operator!");
-        }
-    }
-    
-
+function mod(a,b){
+    return a % b;
 }
 
 function operate(a,b,operator){
@@ -50,7 +24,7 @@ function operate(a,b,operator){
         return add(a,b);
     }
     if(operator == "-"){
-        return subtract(a-b);
+        return subtract(a,b);
     }
     if(operator == "*"){
         return multiply(a,b);
@@ -58,12 +32,54 @@ function operate(a,b,operator){
     if(operator=="/"){
         return divide(a,b);
     }
+    if(operator=="%"){
+        return mod(a,b);
+    }
 }
 
+let start = true;
+const operators = ["+", "-", "/", "*", "%"];
+const container = document.querySelector("#display");
 
-// let a = askNumInput();
-// let b = askNumInput();
-// let operator = askOpInput();
+let operand1 = 0, operand2 = 0, operator = null;
+let hasOperator = false;
+function evaluate(id){
+    if(start){
+        container.textContent = null;
+        start=false;
+    }
 
 
-// alert(operate(a,b,operator));
+    if(hasOperator){
+        container.textContent = null;
+        hasOperator = false;
+    }
+    
+    if(operators.includes(id)){
+        operand1 = container.textContent;
+        operator = id;
+        container.textContent = null;
+        hasOperator = true;
+    }
+    if(id == "="){
+        operand2 = container.textContent;
+        operand1 = parseInt(operand1), operand2 = parseInt(operand2);
+        let x = operate(operand1, operand2, operator);
+        container.textContent = x;
+    }
+    else{
+        container.textContent += id;
+    }
+    if(id == "AC"){
+        container.textContent = 0;
+        start = true;
+    }
+    
+}
+
+const buttons = document.querySelectorAll('div div')
+buttons.forEach((button) => {
+    button.addEventListener('click', function(){
+        evaluate(button.id);
+    });
+});
